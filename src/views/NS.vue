2d9acf71ebf835a4f1c1ab092df71a0cc159e2ba
Cao <Cao20010928@163.com>
2022-12-03 17:33:14 +0800

    学者门户静态页面

--- src/views/NS.vue
+++ src/views/NS.vue
@@ -5,24 +5,150 @@
                 <div class="blackBox">
                     <img src="../assets/YAN.jpg" class="image">
                     <div class="userName" style="position: absolute; text-align: center;
-                     font-size: 45px; display: inline-block; left: 140px; right: 0;
-                     top: 75px; color: white; font-weight: bold">王海涛</div>
+                     font-size: 30px; display: inline-block; left: 85px; right: 0;
+                     top: 30px; color: white; font-weight: bold">王海涛</div>
                 </div>
                 <i class="bx bxs-user-plus"></i>
-                <div style="position: absolute; margin-top: 22px; left: 70px; font-size: 25px">关注</div>
+                <div style="position: absolute; margin-top: 13px; left: 50px; font-size: 15px">关注</div>
                 <i class="bx bxs-award"></i>
-                <div style="position: absolute; margin-top: 22px; left: 220px; font-size: 25px;
+                <div style="position: absolute; margin-top: 13px; left: 135px; font-size: 15px;
                  color: #ffcb74">已认领</div>
                 <i class="bx bxs-chat"></i>
-                <div style="position: absolute; margin-top: 22px; left: 410px; font-size: 25px">私信</div>
+                <div style="position: absolute; margin-top: 13px; left: 245px; font-size: 15px">私信</div>
+            </div>
+            <div class="introduction">
+                <i class="bx bxs-user-pin"></i>
+                <div class="intro">个人简介</div>
+                <div class="line"></div>
+                <div class="detail">这个人很懒，什么都没有写……</div>
+            </div>
+            <div class="name">
+                <i class='bx bxs-thermometer'></i>
+                <div class="heat">热度</div>
+                <div class="line"></div>
+                <div style="position: absolute; text-align: center; width: 100%; top: 70px;
+                 font-size: 30px; color: #C34A36">38</div>
+            </div>
+            <div class="name">
+                <i class='bx bx-globe'></i>
+                <div class="heat">门户访问量</div>
+                <div class="line2"></div>
+                <div style="position: absolute; text-align: center; width: 100%; top: 70px;
+                 font-size: 30px; color: #ffc75f">38</div>
             </div>
         </div>
         <div class="middle">
-
+            <div class="top" style="position: relative; height: 60px">
+                <i class='bx bxs-graduation'></i>
+                <div style="position: absolute; left: 75px; font-size: 25px; font-weight: bold; top: 5px">学术成果</div>
+                <div class="line" style="margin-top: 10px"></div>
+            </div>
+            <div style="position: relative; height: 30px">
+                <div style="position: absolute; left: 20px">排序方式：</div>
+                <button class="sort" @click="">按发表时间排序</button>
+                <button class="sort" style="left: 220px" @click="">按引用量排序</button>
+            </div>
+            <div class="contents">
+                <i class='bx bxs-bookmark-alt'></i>
+                <div class="title">改进的二分法查找</div>
+                <div class="author">王海涛， 朱洪 - 计算机工程, 2006 - cqvip.com</div>
+                <i class='bx bx-link'></i>
+                <div class="citation">被引用次数: 27</div>
+            </div>
+            <div class="contents">
+                <i class='bx bxs-bookmark-alt'></i>
+                <div class="title">改进的二分法查找</div>
+                <div class="author">王海涛， 朱洪 - 计算机工程, 2006 - cqvip.com</div>
+                <i class='bx bx-link'></i>
+                <div class="citation">被引用次数: 27</div>
+            </div>
+            <div class="contents">
+                <i class='bx bxs-bookmark-alt'></i>
+                <div class="title">改进的二分法查找</div>
+                <div class="author">王海涛， 朱洪 - 计算机工程, 2006 - cqvip.com</div>
+                <i class='bx bx-link'></i>
+                <div class="citation">被引用次数: 27</div>
+            </div>
+            <div class="contents">
+                <i class='bx bxs-bookmark-alt'></i>
+                <div class="title">改进的二分法查找</div>
+                <div class="author">王海涛， 朱洪 - 计算机工程, 2006 - cqvip.com</div>
+                <i class='bx bx-link'></i>
+                <div class="citation">被引用次数: 27</div>
+            </div>
+            <div class="contents">
+                <i class='bx bxs-bookmark-alt'></i>
+                <div class="title">改进的二分法查找</div>
+                <div class="author">王海涛， 朱洪 - 计算机工程, 2006 - cqvip.com</div>
+                <i class='bx bx-link'></i>
+                <div class="citation">被引用次数: 27</div>
+            </div>
+            <div class="contents">
+                <i class='bx bxs-bookmark-alt'></i>
+                <div class="title">改进的二分法查找</div>
+                <div class="author">王海涛， 朱洪 - 计算机工程, 2006 - cqvip.com</div>
+                <i class='bx bx-link'></i>
+                <div class="citation">被引用次数: 27</div>
+            </div>
+            <div class="contents">
+                <i class='bx bxs-bookmark-alt'></i>
+                <div class="title">改进的二分法查找</div>
+                <div class="author">王海涛， 朱洪 - 计算机工程, 2006 - cqvip.com</div>
+                <i class='bx bx-link'></i>
+                <div class="citation">被引用次数: 27</div>
+            </div>
+            <div class="contents">
+                <i class='bx bxs-bookmark-alt'></i>
+                <div class="title">改进的二分法查找</div>
+                <div class="author">王海涛， 朱洪 - 计算机工程, 2006 - cqvip.com</div>
+                <i class='bx bx-link'></i>
+                <div class="citation">被引用次数: 27</div>
+            </div>
         </div>
         <div class="right">
+            <div class="top" style="position: relative; height: 60px">
+                <i class='bx bxs-contact'></i>
+                <div style="position: absolute; left: 75px; font-size: 25px; font-weight: bold; top: 5px">学者关系网</div>
+                <div class="line" style="margin-top: 10px"></div>
+            </div>
+            <div class="scholar">
+                <img src="../assets/YAN.jpg" class="image"
+                     style="height: 60px; width: 60px; margin: 5px;
+                padding: 0; border-radius: 8px">
+                <div style="position: absolute; left: 70px;
+                margin-top: 10px; font-weight: bold">YAN</div>
+                <div style="position: absolute; left: 70px; font-size: 10px; bottom: 10px">合作论文数：26</div>
 
+            </div>
+            <div class="scholar">
+                <img src="../assets/YAN.jpg" class="image"
+                     style="height: 60px; width: 60px; margin: 5px;
+                padding: 0; border-radius: 8px">
+                <div style="position: absolute; left: 70px;
+                margin-top: 10px; font-weight: bold">YAN</div>
+                <div style="position: absolute; left: 70px; font-size: 10px; bottom: 10px">合作论文数：26</div>
+
+            </div>
+            <div class="scholar">
+                <img src="../assets/YAN.jpg" class="image"
+                     style="height: 60px; width: 60px; margin: 5px;
+                padding: 0; border-radius: 8px">
+                <div style="position: absolute; left: 70px;
+                margin-top: 10px; font-weight: bold">YAN</div>
+                <div style="position: absolute; left: 70px; font-size: 10px; bottom: 10px">合作论文数：26</div>
+
+            </div>
+            <div class="scholar">
+                <img src="../assets/YAN.jpg" class="image"
+                style="height: 60px; width: 60px; margin: 5px;
+                padding: 0; border-radius: 8px">
+                <div style="position: absolute; left: 70px;
+                margin-top: 10px; font-weight: bold">YAN</div>
+                <div style="position: absolute; left: 70px; font-size: 10px; bottom: 10px">合作论文数：26</div>
+
+            </div>
         </div>
+
     </div>
 
 </template>
@@ -37,67 +163,221 @@
     .left {
         position: relative;
         display: inline-block;
-        width: 25%;
-        height: 1000px;
-        background-color: #4DA5FF;
+        width: 23%;
+        margin: 10px;
+        height: 750px;
+        /*background-color: #4DA5FF;*/
     }
     .middle {
         position: relative;
         display: inline-block;
-        width: 50%;
-        height: 1000px;
-        background-color: #00CA97;
+        width: 48%;
+        height: 750px;
+        margin: 10px;
+        /*background-color: #00CA97;*/
+        border-radius: 5px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.2);
+    }
+    .middle .title {
+        position: absolute;
+        margin: 10px;
+        left: 25px;
+        color: #4DA5FF;
+    }
+    .right .bxs-contact {
+        position: absolute;
+        /*margin-top: 10px;*/
+        margin-left: 20px;
+        font-size: 50px;
+        color: #845ec2;
+    }
+    .right .scholar {
+        position: relative;
+        border-radius: 5px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.2);
+        height: 70px;
+        width: 95%;
+        margin: 5px auto;
+    }
+    .bxs-graduation {
+        position: absolute;
+        /*margin-top: 10px;*/
+        margin-left: 20px;
+        font-size: 50px;
+        color: #FBBD08;
+    }
+    .contents .bx-link {
+        position: absolute;
+        bottom: 10px;
+        right: 95px;
+    }
+    .contents .citation {
+        position: absolute;
+        bottom: 10px;
+        right: 10px;
+        font-size: 10px;
+    }
+    .middle .bxs-bookmark-alt {
+        position: absolute;
+        font-size: 20px;
+        margin: 10px;
+        color: #FBBD08;
+    }
+    .middle .author {
+        position: absolute;
+        margin-left: 10px;
+        bottom: 10px;
+        font-size: 10px;
+        color: #24bf24;
+    }
+    .middle .sort {
+        position: absolute;
+        background-color: white;
+        color: #0f62fe;
+        border: 1px solid #0f62fe;
+        left: 100px;
+        transition: .5s;
+    }
+    .middle .sort:hover {
+        color: white;
+        background: #2196f3;
+    }
+    .middle .contents {
+        position: relative;
+        height: 70px;
+        width: 97%;
+        margin: 5px auto;
+        border-radius: 10px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.2);
     }
     .right {
         position: relative;
         display: inline-block;
-        width: 25%;
-        height: 1000px;
-        background-color: #0fc70f;
+        width: 24%;
+        height: 750px;
+        margin: 10px;
+        border-radius: 5px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.2);
     }
     .left .name {
         position: relative;
         background-color: white;
         width: 100%;
-        height: 280px;
+        height: 145px;
         border-radius: 10px;
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.2);
+        margin-bottom: 10px;
+    }
+    .left .introduction {
+        position: relative;
+        background-color: white;
+        width: 100%;
+        height: 170px;
+        border-radius: 10px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.2);
+        margin-bottom: 10px;
     }
     .name .bxs-user-plus {
         position: absolute;
         display: inline-block;
-        font-size: 40px;
-        margin-top: 20px;
-        margin-left: 20px;
+        font-size: 30px;
+        margin-top: 7px;
+        margin-left: 15px;
         color: #333333;
+    }
+    .introduction .bxs-user-pin {
+        position: absolute;
+        margin-top: 15px;
+        margin-left: 20px;
+        font-size: 30px;
+        color: #0089ba;
+    }
+    .introduction .intro {
+        position: absolute;
+        margin-top: 13px;
+        left: 55px;
+        font-size: 20px;
+        font-weight: bold;
+    }
+    .line {
+        position: relative;
+        width: 80%;
+        background-color: #0081cf;
+        margin-left: 20px;
+        height: 1px;
+        top: 48px;
+    }
+    .introduction .detail {
+        position: absolute;
+        font-size: 10px;
+        top: 55px;
+        left: 20px;
+    }
+    .name .bxs-thermometer {
+        position: absolute;
+        margin-top: 15px;
+        font-size: 30px;
+        margin-left: 20px;
+        color: #c34a36;
+
+    }
+    .name .bx-globe {
+        position: absolute;
+        margin-top: 15px;
+        font-size: 30px;
+        margin-left: 20px;
+        color: #ffc75f;
+    }
+    .name .line {
+        position: relative;
+        width: 80%;
+        background-color: #C34A36;
+        margin-left: 20px;
+        height: 1px;
+        top: 48px;
+    }
+    .name .line2 {
+        position: relative;
+        width: 80%;
+        background-color: #ffc75f;
+        margin-left: 20px;
+        height: 1px;
+        top: 48px;
+    }
+    .name .heat{
+        position: absolute;
+        margin-top: 13px;
+        left: 55px;
+        font-size: 20px;
+        font-weight: bold;
     }
     .name .bxs-award {
         position: absolute;
         display: inline-block;
-        font-size: 40px;
-        margin-top: 20px;
-        left: 180px;
+        font-size: 30px;
+        margin-top: 7px;
+        left: 105px;
         color: #ffcb74;
     }
     .name .bxs-chat {
         position: absolute;
         display: inline-block;
-        font-size: 40px;
-        margin-top: 20px;
-        left: 360px;
+        font-size: 30px;
+        margin-top: 7px;
+        left: 210px;
         color: #333333;
     }
     .blackBox {
         position: relative;
         background: url("../assets/img/ScholarsPortal/nameBackground.jpg");
-        position: relative; width: 100%; height: 200px;
+        position: relative; width: 100%; height: 100px;
         border-radius: 10px
     }
-    .blackBox .image {
+    .image {
         position: absolute;
-        padding: 30px;
-        width: 140px; height: 140px;
-        border-radius: 10px;
+        padding: 15px;
+        width: 70px; height: 70px;
+        border-radius: 20px;
         display: inline-block
     }
 </style>
