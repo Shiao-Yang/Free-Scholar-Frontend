e03035d49c32aed8217529d506256c43d8d95bbf
ShiaoYang <20372024@buaa.edu.cn>
2022-12-10 18:51:39 +0800

    $store.state添加token

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,8 @@ export default new Vuex.Store({
       email: "",
       profile: "I'm Peter",
     },
-    address: 'http://139.9.134.209:8000/'
+    address: 'http://139.9.134.209:8000/',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiYWRtaW4iOmZhbHNlLCJleHAiOjE2NzA2OTA5OTR9.iDIFfSr49PZ-YZPH3i90HmTsrP2u4pRw_za2cU0hr3Q',
   },
   getters: {
   },
