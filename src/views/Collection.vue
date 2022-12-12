1e5859afa3799ee95e825e8abc35d4cf6e900534
Weng Jiahao <1010495847@qq.com>
2022-12-12 17:04:50 +0800

    收藏夹侧边栏修改，新增编辑按钮

--- src/views/Collection.vue
+++ src/views/Collection.vue
@@ -12,61 +12,102 @@
       <div class="divider"></div>
       <div class="main-container">
         <div class="Collections">
-          <el-collapse v-model="activeNames" @change="handleChange">
-            <el-collapse-item name="1">
-              <template slot="title">
-              <span style="font-size: 18px;margin-left: 20px">
-                我创建的
-              </span>
-              </template>
-              <div class="item" @mouseenter="onNew" @mouseleave="leaveNew" :style="NewStyle">
-                <i class="el-icon-circle-plus" style="margin-right: 5px;margin-left: 10px;"></i><b>新建收藏夹</b>
-              </div>
-              <div v-for="(item,index) in List" :key="index">
-                <div class="item" @mouseenter="onItem(index)" @mouseleave="leaveItem(index)" @click="clickItem(index)" :style="List[index].style">
-                  <div v-if="List[index].isClick === 0">
-                    <i class="bx bxs-folder" style="margin-right: 5px;margin-left: 10px;"></i><b>{{List[index].name}}</b>
-                  </div>
-                  <div v-if="List[index].isClick === 1">
-                    <i class="bx bxs-folder-open" style="margin-right: 5px;margin-left: 10px;color: white"></i><b style="color: white">{{List[index].name}}</b>
-                  </div>
+          <ul class="menu">
+            <li class="menu-item" :class="{'active': activeMenu1}">
+              <div class="menu-title" @click="activeMenu1 = !activeMenu1">
+                <div class="menu-content">
+                  <span class="menu-icon"><i class='bx bxs-bookmarks'></i></span>
+                  <span class="menu-text">我创建的</span>
                 </div>
+                <span class="menu-arrow"><i class='bx bxs-chevron-left' ></i></span>
               </div>
-            </el-collapse-item>
-            <el-collapse-item name="2">
-              <template slot="title">
-              <span style="font-size: 18px;margin-left: 20px">
-                我的订阅与收藏
-              </span>
-              </template>
-              <div v-for="(item,index) in List1" :key="index">
-                <div class="item" @mouseenter="onItem1(index)" @mouseleave="leaveItem1(index)" @click="click1Item(index)" :style="List1[index].style">
-                  <div v-if="List1[index].isClick === 0">
-                    <i class="bx bxs-book-alt" style="margin-right: 5px;margin-left: 10px;"></i><b>{{List1[index].name}}</b>
-                    <span style="float: right;margin-right: 20px">
-                    32
-                  </span>
-                  </div>
-                  <div v-if="List1[index].isClick === 1">
-                    <i class="bx bxs-book-open" style="margin-right: 5px;margin-left: 10px;color: white"></i><b style="color: white">{{List1[index].name}}</b>
-                    <span style="float: right;margin-right: 20px;color: white">
-                    32
-                  </span>
-                  </div>
+              <ul class="sub-menu">
+                <li class="sub-item">
+                  <span class="sub-icon"><i class='bx bx-plus-circle'></i></span>
+                  <span class="sub-text">新建收藏夹</span>
+                  <span class="sub-edit-icon"></span>
+                </li>
+                <div v-for="(item,index) in List" :key="index">
+                  <li class="sub-item" :class="{'active': item.isClick === 1}" @click="clickItem(index)">
+                    <span class="sub-icon" v-if="item.isClick === 0"><i class='bx bxs-folder' ></i></span>
+                    <span class="sub-icon" v-else><i class='bx bxs-folder-open' ></i></span>
+                    <span class="sub-text">{{ item.name }}</span>
+                    <span class="sub-edit-icon"><i class='bx bx-edit-alt' ></i></span>
+                  </li>
                 </div>
-              </div>
-              <div v-for="(item,index) in List2" :key="index">
-                <div class="item" @mouseenter="onItem2(index)" @mouseleave="leaveItem2(index)" @click="click2Item(index)" :style="List2[index].style">
-                  <div v-if="List2[index].isClick === 0">
-                    <i class="bx bxs-folder" style="margin-right: 5px;margin-left: 10px;"></i><b>{{List2[index].name}}</b>
-                  </div>
-                  <div v-if="List2[index].isClick === 1">
-                    <i class="bx bxs-folder-open" style="margin-right: 5px;margin-left: 10px;color: white"></i><b style="color: white">{{List2[index].name}}</b>
-                  </div>
+              </ul>
+            </li>
+            <li class="menu-item" :class="{'active': activeMenu2}" >
+              <div class="menu-title" @click="activeMenu2 = !activeMenu2">
+                <div class="menu-content">
+                  <span class="menu-icon"><i class='bx bxs-collection' ></i></span>
+                  <span class="menu-text">我收藏的</span>
                 </div>
+                <span class="menu-arrow"><i class='bx bxs-chevron-left' ></i></span>
               </div>
-            </el-collapse-item>
-          </el-collapse>
+              <ul class="sub-menu">
+                <li class="sub-item">
+                  <span class="sub-text"></span>
+                  <span class="sub-edit-icon"></span>
+                </li>
+              </ul>
+            </li>
+          </ul>
+<!--          <el-collapse v-model="activeNames" @change="handleChange">-->
+<!--            <el-collapse-item name="1">-->
+<!--              <template slot="title">-->
+<!--              <span style="font-size: 18px;margin-left: 20px">-->
+<!--                我创建的-->
+<!--              </span>-->
+<!--              </template>-->
+<!--              <div class="item" @mouseenter="onNew" @mouseleave="leaveNew" :style="NewStyle">-->
+<!--                <i class="el-icon-circle-plus" style="margin-right: 5px;margin-left: 10px;"></i><b>新建收藏夹</b>-->
+<!--              </div>-->
+<!--              <div v-for="(item,index) in List" :key="index">-->
+<!--                <div class="item" @mouseenter="onItem(index)" @mouseleave="leaveItem(index)" @click="clickItem(index)" :style="List[index].style">-->
+<!--                  <div v-if="List[index].isClick === 0">-->
+<!--                    <i class="bx bxs-folder" style="margin-right: 5px;margin-left: 10px;"></i><b>{{List[index].name}}</b>-->
+<!--                  </div>-->
+<!--                  <div v-if="List[index].isClick === 1">-->
+<!--                    <i class="bx bxs-folder-open" style="margin-right: 5px;margin-left: 10px;color: white"></i><b style="color: white">{{List[index].name}}</b>-->
+<!--                  </div>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--            </el-collapse-item>-->
+<!--            <el-collapse-item name="2">-->
+<!--              <template slot="title">-->
+<!--              <span style="font-size: 18px;margin-left: 20px">-->
+<!--                我的订阅与收藏-->
+<!--              </span>-->
+<!--              </template>-->
+<!--              <div v-for="(item,index) in List1" :key="index">-->
+<!--                <div class="item" @mouseenter="onItem1(index)" @mouseleave="leaveItem1(index)" @click="click1Item(index)" :style="List1[index].style">-->
+<!--                  <div v-if="List1[index].isClick === 0">-->
+<!--                    <i class="bx bxs-book-alt" style="margin-right: 5px;margin-left: 10px;"></i><b>{{List1[index].name}}</b>-->
+<!--                    <span style="float: right;margin-right: 20px">-->
+<!--                    32-->
+<!--                  </span>-->
+<!--                  </div>-->
+<!--                  <div v-if="List1[index].isClick === 1">-->
+<!--                    <i class="bx bxs-book-open" style="margin-right: 5px;margin-left: 10px;color: white"></i><b style="color: white">{{List1[index].name}}</b>-->
+<!--                    <span style="float: right;margin-right: 20px;color: white">-->
+<!--                    32-->
+<!--                  </span>-->
+<!--                  </div>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--              <div v-for="(item,index) in List2" :key="index">-->
+<!--                <div class="item" @mouseenter="onItem2(index)" @mouseleave="leaveItem2(index)" @click="click2Item(index)" :style="List2[index].style">-->
+<!--                  <div v-if="List2[index].isClick === 0">-->
+<!--                    <i class="bx bxs-folder" style="margin-right: 5px;margin-left: 10px;"></i><b>{{List2[index].name}}</b>-->
+<!--                  </div>-->
+<!--                  <div v-if="List2[index].isClick === 1">-->
+<!--                    <i class="bx bxs-folder-open" style="margin-right: 5px;margin-left: 10px;color: white"></i><b style="color: white">{{List2[index].name}}</b>-->
+<!--                  </div>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--            </el-collapse-item>-->
+<!--          </el-collapse>-->
         </div>
         <div class="CollectionItems">
           <div class="CollectionItem">
@@ -312,6 +353,8 @@ export default {
   name: "Collection",
   data() {
     return {
+      activeMenu1: false,
+      activeMenu2: false,
       activeNames: [],
       NewStyle: '',
       List: [
@@ -464,6 +507,12 @@ export default {
 </script>
 
 <style scoped>
+ul {
+  list-style: none;
+}
+li {
+
+}
 .introductions {
   color: #030303;
   font-size: 10px;
@@ -504,7 +553,13 @@ export default {
   border-right-style: solid;
   left: 4px;
   width: 240px;
+  /*height: 100%;*/
 }
+
+.CollectionItems::-webkit-scrollbar {
+  width: 0;
+}
+
 .titleName {
   position: relative;
   top: 25px;
@@ -539,4 +594,119 @@ export default {
 .main-container {
   display: flex;
 }
+
+.Collections .menu {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  transition: all ease 0.4s;
+}
+
+.Collections .menu .menu-item {
+  width: 100%;
+  position: relative;
+  list-style: none;
+  transition:all ease 0.4s;
+}
+
+.Collections .menu .menu-item .menu-arrow {
+  transition: all ease 0.4s;
+}
+
+.Collections .menu .menu-item.active .menu-arrow {
+  transform: rotate(-90deg);
+}
+
+.Collections .menu .menu-item.active .menu-title,
+.Collections .menu .menu-item:hover .menu-title {
+  background-color: rgba(244, 244, 244, 1);
+}
+
+.Collections .menu .menu-item .menu-title {
+  height: 40px;
+  line-height: 40px;
+  font-size: 18px;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+  background-color: rgba(244,244,244, 0.3);
+  transition: 0.4s all ease;
+}
+
+.Collections .menu .menu-item .menu-title:hover {
+  background-color: rgba(244,244,244, 1);
+}
+
+.Collections .menu .menu-item .menu-title .menu-content .menu-icon i{
+  height: 40px;
+  line-height: 40px;
+  margin-right: 10px;
+  text-align: center;
+}
+
+.Collections .menu .menu-item .sub-menu {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: none;
+  opacity: 0;
+  visibility: hidden;
+  width: 100%;
+  transition: all ease 0.4s;
+  pointer-events: none;
+}
+
+.Collections .menu .menu-item.active .sub-menu {
+  opacity: 1;
+  visibility: visible;
+  display: inline-block;
+  pointer-events: auto;
+}
+
+.menu-item .sub-menu .sub-item{
+  margin: 10px 20px;
+  height: 30px;
+  line-height: 30px;
+  display: flex;
+  font-size: 14px;
+  text-align: center;
+  justify-items: center;
+  background-color: #f4f4f4;
+  border-radius: 20px;
+  cursor: pointer;
+}
+
+.menu-item .sub-menu .sub-item.active,
+.menu-item .sub-menu .sub-item:hover {
+  background-color: #2196f3;
+  color: white;
+}
+
+.menu-item .sub-menu .sub-item .sub-icon {
+  min-width: 30px;
+}
+
+.menu-item .sub-menu .sub-item .sub-text {
+  overflow: hidden;
+}
+
+
+.menu-item .sub-menu .sub-item .sub-edit-icon {
+  margin-left: auto;
+  opacity: 0;
+  visibility: hidden;
+  pointer-events: none;
+  transition: all ease 0.4s;
+}
+
+.menu-item .sub-menu .sub-item:hover .sub-edit-icon{
+  margin-right: 10px;
+  opacity: 1;
+  visibility: visible;
+  pointer-events: auto;
+}
+
 </style>
